1420b994012cb6eb5f36ce521751b04a5d42ba31
mudassir uddin <114720689+mudassiruddin-coding@users.noreply.github.com>
2024-01-10 15:08:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Programmer", "Developer", "Student", "Data Analytics", ],
+        strings: ["Programmer", "Developer", "Data Analytics"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Programmer", "Developer", "Student", "Data Analytics", ],
+        strings: ["Programmer", "Developer", "Data Analytics"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
